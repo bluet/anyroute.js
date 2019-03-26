7bd74d948bd0ec37a158142da0a91d796a596cad
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2019-03-27 06:46:28 +0800

    fix typo and quote

--- example.js
+++ example.js
@@ -1,29 +1,29 @@
-var anyroute = require('anyroute');
+var anyroute = require("anyroute");
 var ar = new anyroute;
 
-function handler_default () {};
-function handler_post () {};
+function handler_default () {}
+function handler_post () {}
 var ret;
 
-ret = ar.set('/collection/:cid/tab/:tabID', handler_default);
+ret = ar.set("/collection/:cid/tab/:tabID", handler_default);
 console.log(ret);
 
-ret = ar.set('/collection/:cid/tab/:tabID', handler_default, 'default');
+ret = ar.set("/collection/:cid/tab/:tabID", handler_default, "default");
 console.log(ret);
 
-ret = ar.set('/collection/:cid/tab/:tabID/', handler_post, 'post')
+ret = ar.set("/collection/:cid/tab/:tabID/", handler_post, "post");
 console.log(ret);
 
-console.log('--------------------');
+console.log("--------------------");
 
-ret = ar.get('/collection/:cid/tab/:tabID');
+ret = ar.get("/collection/:cid/tab/:tabID");
 console.log(ret);
 
-ret = ar.get('/collection/123/tab/456', 'default');
+ret = ar.get("/collection/123/tab/456", "default");
 console.log(ret);
 
-ret = ar.get('/collection/CCC:ccc/tab/Tab:tabID', 'post');
+ret = ar.get("/collection/CCC:ccc/tab/Tab:tabID", "post");
 console.log(ret);
 
-ret = ar.get('/collection/foo/tab/bar', 'all');
+ret = ar.get("/collection/foo/tab/bar", "all");
 console.log(ret);
