cbef54e392944afcbf2e38d5e9d4d13c421b378f
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2022-12-15 11:17:45 +0800

    add dotfiles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,57 @@
 module.exports = {
-	'env': {
-		'es6': true,
-		'node': true
+	"env": {
+		"es2020": true,
+		"node": true,
+		"browser": true,
+		"shared-node-browser": true,
+		"jest": true,
+		"mongo": true,
 	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 2017,
-		'sourceType': 'module'
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 11,
+		"sourceType": "module",
+		"requireConfigFile": false,
 	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab',
-			{ 'SwitchCase': 1 },
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'double'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'no-console': [
-			'warn',
-			{ allow: ['warn', 'error', 'info'] }
-		],
-		'brace-style': [
-			'error',
-			'1tbs',
-			{ 'allowSingleLine': true }
-		],
-		'no-unused-vars': [
-			'warn',
+	"rules": {
+		"indent": ["error", "tab", { "VariableDeclarator": "first" }],
+		"linebreak-style": ["error", "unix"],
+		"curly": "error",
+		"quotes": [
+			"warn",
+			"double",
 			{
-				'vars': 'all',
-				'args': 'after-used',
-				'ignoreRestSiblings': false
-			}
+				"avoidEscape": true,
+				"allowTemplateLiterals": true,
+			},
 		],
-		'no-mixed-spaces-and-tabs': [
-			'error',
-			'smart-tabs'
-		]
-	}
+		"quote-props": ["error", "always"],
+		"semi": ["error", "always"],
+		"no-console": ["warn", { "allow": ["warn", "error", "info"] }],
+		"brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+		"no-unused-vars": [
+			"warn",
+			{
+				"vars": "all",
+				"args": "after-used",
+				"ignoreRestSiblings": false,
+			},
+		],
+		"arrow-parens": ["error", "always"],
+		"arrow-body-style": ["warn", "always"],
+		"func-style": ["warn", "declaration", { "allowArrowFunctions": true }],
+		"no-confusing-arrow": ["error", { "allowParens": false }],
+		"prefer-arrow-callback": "warn",
+		"require-await": "error",
+		"operator-linebreak": ["warn", "before"],
+		"no-constant-condition": ["error", { "checkLoops": false }],
+		"no-throw-literal": "warn",
+		"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+		"no-trailing-spaces": "error",
+		"no-multi-spaces": "error",
+		"arrow-spacing": "error",
+		"keyword-spacing": ["error", { "after": true, "before": true }],
+		"space-before-blocks": "error",
+		"space-before-function-paren": ["error", "always"],
+	},
 };
